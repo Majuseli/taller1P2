445a3f67625602af2f53c6d39cdf12eb6f186fdd
Majuseli <chinchovertel@gmail.com>
2026-04-02 22:54:13 -0500

    Creacion de metodos concretos en la clase Cuenta peron sin lso exception y clase transaccion

--- src/modelo/abstractas/Cuenta.java
+++ src/modelo/abstractas/Cuenta.java
@@ -10,14 +10,62 @@ public abstract class Cuenta {
     private LocalDateTime fechaCreacion;
     private LocalDateTime ultimaModificacion;
     private String usuarioModificacion;
-    private Object[] historial = new Object[20]; //CAMBIAR POR TRANSACCION DESPUES DE CREARLA
+    private Object[ ] historial = new Object[20]; //CAMBIAR POR TRANSACCION DESPUES DE CREARLA
     private int cont = 0;
     
     
+    //CONSTRUCTOR
+    public Cuenta (String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.bloqueada = false;
+        this.fechaCreacion = LocalDateTime.now();
+    
+    }
     
     
     
     
+    //GETTERS
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean isBloqueada() {
+        return bloqueada;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+    
+    
+    
+    //METODOS CONCRETOS
+    public void verificarBloqueada(){
+        if (bloqueada) {
+            System.out.println("Cuenta Bloqueada"); //AQUI EXCEPTION CuentaBloqueadaException
+        }
+    }
+    
+    
+    public void agregarAlHistorial(Object i) {  //CAMBIAR POR TRANSACCION DESPUES DE CREARLA
+        if (cont >= historial.length) {
+            System.out.println("Historial lleno"); //AQUI EXCEPTION CuentaBloqueadaException
+        }
+        historial[cont++] = i;
+    }
+
+    public Object[ ] getHistorial() {
+        return historial.clone(); //DEVUELVE UNA COPIA
+    }
+    
+    
+ 
     
     //METODOS ABSTRACTOS
     public abstract double calcularInteres();
