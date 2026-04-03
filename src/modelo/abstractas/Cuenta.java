58ed0dc92319e14ad0d5e4762f7e9d8c403d78ba
Majuseli <chinchovertel@gmail.com>
2026-04-02 21:24:38 -0500

    Atributos y metodos abstractos agregados a la clase Cuenta

--- src/modelo/abstractas/Cuenta.java
+++ src/modelo/abstractas/Cuenta.java
@@ -1,6 +1,29 @@
-
 package modelo.abstractas;
 
-public class Cuenta {
+import java.time.LocalDateTime;
+
+public abstract class Cuenta {
+    
+    private String numeroCuenta;
+    private double saldo;
+    private boolean bloqueada;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime ultimaModificacion;
+    private String usuarioModificacion;
+    private Object[] historial = new Object[20]; //CAMBIAR POR TRANSACCION DESPUES DE CREARLA
+    private int cont = 0;
+    
+    
+    
+    
+    
+    
+    
+    //METODOS ABSTRACTOS
+    public abstract double calcularInteres();
+    
+    public abstract double getLimiteRetiro();
+    
+    public abstract String getTipoCuenta();
     
 }
