3cd1eec1afc724f95d3e4636be3f986aa1a9f3d8
Majuseli <chinchovertel@gmail.com>
2026-04-02 19:34:01 -0500

    Atributos agregados a la clase empleado y uso de extend

--- src/modelo/abstractas/Empleado.java
+++ src/modelo/abstractas/Empleado.java
@@ -1,7 +1,13 @@
-
 package modelo.abstractas;
 
+import java.time.LocalDate;
 
-public class Empleado {
+
+public abstract class Empleado extends Persona {
+    
+    private String legajo;
+    private LocalDate fechaContratacion;
+    private double salarioBase;
+    private boolean activo;
     
 }
