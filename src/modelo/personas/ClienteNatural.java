b33a8afd8f796c24c527e90ee59da3332ef9555a
Majuseli <chinchovertel@gmail.com>
2026-04-04 00:57:13 -0500

    Atributos, constructor y metodos abstractos agregados a la clase ClienteNatural y creacion de la clase main para ver resultados hasta el momento

--- src/modelo/personas/ClienteNatural.java
+++ src/modelo/personas/ClienteNatural.java
@@ -1,5 +1,39 @@
 package modelo.personas;
+import java.time.LocalDate;
+import modelo.abstractas.Persona;
 
-public class ClienteNatural {
+public class ClienteNatural extends Persona{
+    
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private Object[ ] cuentas = new Object[5]; //CAMBIAR POR CUENTA DESPUES DE TERMINARLA 100%
+    private int cont = 0;
+    
+    
+    
+    public ClienteNatural (String id, String nombre, String apellido, LocalDate fechaNacimiento, String email,
+                          String tipoDocumento, String numeroDocumento) {
+
+        super(id, nombre, apellido, fechaNacimiento, email);
+        
+    }
+    
+    
+    
+    //METODOS ABSTRACTOS DE LA CLASE PADRE
+    @Override
+    public int calcularEdad() {
+        return LocalDate.now().getYear() - getFechaNacimiento().getYear();
+    }
+
+    @Override
+    public String obtenerTipo() {
+        return "Cliente Natural";
+    }
+
+    @Override
+    public String obtenerDocumentoIdentidad(){
+        return tipoDocumento + numeroDocumento;
+    }
     
 }
