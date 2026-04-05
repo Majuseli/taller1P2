8cac5caaaca4c7978a7ce6cd2df596357174a4d8
Majuseli <chinchovertel@gmail.com>
2026-04-05 15:37:54 -0500

    Desarrolo de la clase CuentaAhorro con calculos de interes y herencia pero sin las interfaces

--- src/modelo/cuentas/CuentaAhorro.java
+++ src/modelo/cuentas/CuentaAhorro.java
@@ -1,5 +1,38 @@
 package modelo.cuentas;
 
-public class CuentaAhorro {
+import modelo.abstractas.Cuenta;
+
+public class CuentaAhorro extends Cuenta {
+    
+    private double tasaInteres;
+    private int retirosMesActual;
+    private int maxRetirosMes;
+    
+    
+    //CONTROLADOR
+    public CuentaAhorro(String numeroCuenta, double saldo, double tasaInteres, int maxRetirosMes) {
+
+        super(numeroCuenta, saldo);
+
+        this.tasaInteres = tasaInteres;
+        this.maxRetirosMes = maxRetirosMes;
+        this.retirosMesActual = 0;
+    }
+    
+    
+    @Override
+    public double calcularInteres() {
+        return getSaldo() * tasaInteres / 12;
+    }
+
+    @Override
+    public double getLimiteRetiro() {
+        return 1000; 
+    }
+
+    @Override
+    public String getTipoCuenta() {
+        return "Ahorros";
+    }
     
 }
