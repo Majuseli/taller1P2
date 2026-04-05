aff8327d7b3dfdd8dfe551980ec286382cd4ffbf
Majuseli <chinchovertel@gmail.com>
2026-04-05 16:26:44 -0500

    Implementacion clase CuentaCorriente solo con sbregiro y comision

--- src/modelo/cuentas/CuentaCorriente.java
+++ src/modelo/cuentas/CuentaCorriente.java
@@ -1,5 +1,37 @@
 package modelo.cuentas;
 
-public class CuentaCorriente {
+import modelo.abstractas.Cuenta;
+
+public class CuentaCorriente extends Cuenta {
+    
+    private double montoSobregiro;
+    private double comisionMantenimiento;
+    
+    
+    public CuentaCorriente(String numeroCuenta, double saldo, double montoSobregiro, double comisionMantenimiento) {
+
+        super(numeroCuenta, saldo);
+
+        this.montoSobregiro = montoSobregiro;
+        this.comisionMantenimiento = comisionMantenimiento;
+    }
+    
+    
+    
+    
+    @Override
+    public double calcularInteres() {
+        return 0; 
+    }
+
+    @Override
+    public double getLimiteRetiro() {
+        return getSaldo() + montoSobregiro;
+    }
+
+    @Override
+    public String getTipoCuenta() {
+        return "Corriente";
+    }
     
 }
