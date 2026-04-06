354cc8cbc937cc2937003a4e6e33be3454a51676
Majuseli <chinchovertel@gmail.com>
2026-04-05 23:34:38 -0500

    Implementacion de interfaces y gestion correcta de cuenta a la clase ClienteEmpresarial

--- src/modelo/personas/ClienteEmpresarial.java
+++ src/modelo/personas/ClienteEmpresarial.java
@@ -2,15 +2,23 @@ package modelo.personas;
 
 import java.time.LocalDate;
 import modelo.abstractas.Persona;
+import modelo.interfaces.*;
+import java.time.LocalDateTime;
 
  
-public class ClienteEmpresarial extends Persona{
+public class ClienteEmpresarial extends Persona implements Consultable, Notificable, Auditable{
     private String nit;
     private String razonSocial;
     private String representanteLegal;
     private Object[] cuentas = new Object[5]; //CAMBIAR POR CUENTA DESPUES DE TERMINARLA 100%
     private int cont = 0;
     
+    //ATRIBUTOS DE  AUDITABLE Y MODIFICABLE
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime ultimaModificacion;
+    private String usuarioModificacion;
+    private boolean aceptaNotificaciones;
+
     
     
     //CONTADOR
@@ -22,6 +30,8 @@ public class ClienteEmpresarial extends Persona{
         this.nit = nit;
         this.razonSocial = razonSocial;
         this.representanteLegal = representanteLegal;
+        this.fechaCreacion = LocalDateTime.now();
+        this.aceptaNotificaciones = true;
         
     }
     
@@ -38,9 +48,12 @@ public class ClienteEmpresarial extends Persona{
     
     
     public Object[] getCuentas() {
-        return cuentas.clone();
-    }
+        Object[] copia = new Object[cont];
     
+        System.arraycopy(cuentas, 0, copia, 0, cont);
+        
+        return copia;
+    }
     
     
     //METODOS ABSTRACTOS
@@ -57,6 +70,59 @@ public class ClienteEmpresarial extends Persona{
     @Override
     public String obtenerDocumentoIdentidad(){
         return   nit;
+    }
+    
+    
+    
+    //METODOS CONSULTABLE
+    @Override
+    public String obtenerResumen() {
+        return razonSocial + " - NIT: " + nit;
+    }
+
+    @Override
+    public boolean estaActivo() {
+        return true;
+    }
+
+    // METODOS NOTIFICABLE
+    @Override
+    public void notificar(String mensaje) {
+        if (aceptaNotificaciones) {
+            System.out.println("Notificación: " + mensaje);
+        }
+    }
+
+    @Override
+    public String obtenerContacto() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean aceptaNotificaciones() {
+        return aceptaNotificaciones;
+    }
+
+    // METODOS AUDITABLE
+    @Override
+    public LocalDateTime obtenerFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    @Override
+    public LocalDateTime obtenerUltimaModificacion() {
+        return ultimaModificacion;
+    }
+
+    @Override
+    public String obtenerUsuarioModificacion() {
+        return usuarioModificacion;
+    }
+
+    @Override
+    public void registrarModificacion(String usuario) {
+        this.usuarioModificacion = usuario;
+        this.ultimaModificacion = LocalDateTime.now();
     }
         
     
